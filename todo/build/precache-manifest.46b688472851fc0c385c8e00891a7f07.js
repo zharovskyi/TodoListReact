e0058e2a3793ffd3b92526139c1188ec3dddaa6f
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-10-04 10:29:54 +0300

    add filter method arr

---
+++ todo/build/precache-manifest.46b688472851fc0c385c8e00891a7f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a08086b3c0babaeb13be62f8bdc72db",
+    "url": "/TodoListReact/todo/build/index.html"
+  },
+  {
+    "revision": "c66ab4cd5ea957e50d4d",
+    "url": "/TodoListReact/todo/build/static/css/main.911a1894.chunk.css"
+  },
+  {
+    "revision": "8381e76f5a39ecaf5506",
+    "url": "/TodoListReact/todo/build/static/js/2.0686d2b1.chunk.js"
+  },
+  {
+    "revision": "c66ab4cd5ea957e50d4d",
+    "url": "/TodoListReact/todo/build/static/js/main.fa1fa0ef.chunk.js"
+  },
+  {
+    "revision": "0a7e0f3f36d5429e1abc",
+    "url": "/TodoListReact/todo/build/static/js/runtime-main.4592f712.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/TodoListReact/todo/build/static/media/logo.25bf045c.svg"
+  }
+]);
